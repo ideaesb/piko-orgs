5b059891d51528553b42a8f86e5d59656998d4ac
IDEA ESB <ideaesb@gmail.com>
2012-09-20 22:27:29 -1000

    and logic for Lucene, x of total below listing

--- src/main/java/org/ideademo/orgs/pages/Index.java
+++ src/main/java/org/ideademo/orgs/pages/Index.java
@@ -1,12 +1,15 @@
 package org.ideademo.orgs.pages;
 
+
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.StringTokenizer;
 import java.util.Vector;
 
 import org.apache.tapestry5.PersistenceConstants;
 
+import org.apache.tapestry5.annotations.PageActivationContext;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.Persist;
 
@@ -23,7 +26,11 @@ import org.hibernate.criterion.MatchMode;
 
 import org.hibernate.search.FullTextSession;
 import org.hibernate.search.Search;
+
+import org.hibernate.search.query.dsl.BooleanJunction;
 import org.hibernate.search.query.dsl.QueryBuilder;
+import org.hibernate.search.query.dsl.TermMatchingContext;
+
 
 import org.ideademo.orgs.entities.Org;
 
@@ -60,7 +67,12 @@ public class Index
   @Inject
   private HibernateSessionManager sessionManager;
 
-  
+  @Property 
+  @Persist (PersistenceConstants.FLASH)
+  int retrieved; 
+  @Property 
+  @Persist (PersistenceConstants.FLASH)
+  int total;
   
   ////////////////////////////////////////////////////////////////////////////////////////////////////////
   //  Entity List generator - QBE, Text Search or Show All 
@@ -76,8 +88,13 @@ public class Index
 	logger.info("Search Text = " + searchText);
 	
 	
-	// then makes lists and sublists as per the search criteria 
-	List<Org> xlst=null; // xlst = Query by Example search List - not used for Orgs
+    // Get all records anyway - for showing total at bottom of presentation layer
+    List <Org> alst = session.createCriteria(Org.class).list();
+    total = alst.size();
+
+	
+    // then makes lists and sublists as per the search criteria 
+    List<Org> xlst=null; // xlst = Query by Example search List
     if(example != null)
     {
        Example ex = Example.create(example).excludeFalse().ignoreCase().enableLike(MatchMode.ANYWHERE);
@@ -87,15 +104,15 @@ public class Index
        
        if (xlst != null)
        {
-    	   logger.info("Example Search Result List Size  = " + xlst.size() );
+    	   logger.info("Org Example Search Result List Size  = " + xlst.size() );
     	   Collections.sort(xlst);
        }
        else
        {
-         logger.info("Example Search result did not find any results...");
+         logger.info("Org Example Search result did not find any results...");
        }
     }
-
+    
     List<Org> tlst=null;
     if (searchText != null && searchText.trim().length() > 0)
     {
@@ -110,12 +127,21 @@ public class Index
        }
       
        QueryBuilder qb = fullTextSession.getSearchFactory().buildQueryBuilder().forEntity( Org.class ).get();
-       org.apache.lucene.search.Query luceneQuery = qb
-			    .keyword()
-			    .onFields("code","name","description", "keywords","contact", "url", "worksheet", "logo")
-			    .matching(searchText)
-			    .createQuery();
-      	  
+       
+       // fields being covered by text search 
+       TermMatchingContext onFields = qb
+		        .keyword()
+		        .onFields("code","name","description", "keywords","contact", "url", "worksheet", "logo");
+       
+       BooleanJunction<BooleanJunction> bool = qb.bool();
+       /////// Tokenize the search string for default AND logic ///
+       StringTokenizer st = new StringTokenizer(searchText);
+       while (st.hasMoreElements()) {
+    	   bool.must(onFields.matching(st.nextElement()).createQuery());
+       }
+       
+       org.apache.lucene.search.Query luceneQuery = bool.createQuery();
+       
        tlst = fullTextSession.createFullTextQuery(luceneQuery, Org.class).list();
        if (tlst != null) 
        {
@@ -133,44 +159,68 @@ public class Index
     if (example == null && (searchText == null || searchText.trim().length() == 0))
     {
     	// Everything...
-    	List <Org> alst = session.createCriteria(Org.class).list(); // alst = List of all records 
     	if (alst != null && alst.size() > 0)
     	{
-    		logger.info ("Returing all " + alst.size() + " Org records");
+    		logger.info ("Returing all " + alst.size() + " Orgs records");
         	Collections.sort(alst);
     	}
     	else
     	{
     		logger.warn("No Org records found in the database");
     	}
+    	retrieved = total;
         return alst; 
     }
     else if (xlst == null && tlst != null)
     {
     	// just text search results
-    	logger.info("Returing " + tlst.size() + " records as a result of PURE text search (no QBE) for " + searchText);
+    	logger.info("Returing " + tlst.size() + " Orgs records as a result of PURE text search (no QBE) for " + searchText);
+    	retrieved = tlst.size();
     	return tlst;
     }
     else if (xlst != null && tlst == null)
     {
     	// just example query results
-    	logger.info("Returning " + xlst.size() + " records as a result ofPURE Query-By-Example (QBE), no text string");
+    	logger.info("Returning " + xlst.size() + " Orgs records as a result of PURE Query-By-Example (QBE), no text string");
+    	retrieved = xlst.size();
     	return xlst;
     }
     else 
     {
+
+        ////////////////////////////////////////////
     	// get the INTERSECTION of the two lists
     	
     	// TRIVIAL: if one of them is empty, return the other
-    	if (xlst.size() == 0 && tlst.size() > 0) return tlst;
-    	if (tlst.size() == 0 && xlst.size() > 0) return xlst;
+    	// if one of them is empty, return the other
+    	if (xlst.size() == 0 && tlst.size() > 0)
+    	{
+        	logger.info("Returing " + tlst.size() + " Orgs records as a result of ONLY text search, QBE pulled up ZERO records for " + searchText);
+        	retrieved = tlst.size();
+    		return tlst;
+    	}
+
+    	if (tlst.size() == 0 && xlst.size() > 0)
+    	{
+        	logger.info("Returning " + xlst.size() + " Orgs records as a result of ONLY Query-By-Example (QBE), text search pulled up NOTHING for string " + searchText);
+        	retrieved = xlst.size();
+	        return xlst;
+    	}
     	
     	
     	List <Org> ivec = new Vector<Org>();
     	// if both are empty, return this Empty vector. 
-    	if (xlst.size() == 0 && tlst.size() == 0) return ivec; 
+    	if (xlst.size() == 0 && tlst.size() == 0)
+    	{
+        	logger.info("Neither QBE nor text search for string " + searchText +  " pulled up ANY Orgs Records.");
+        	retrieved = 0;
+    		return ivec;
+    	}
     	
-    	// now deal with BOTH text and QBE being non-empty lists - by Id
+
+
+    	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    	// now deal with BOTH text and QBE being non-empty lists - implementing intersection by Database Primary Key -  Id
     	Iterator<Org> xiterator = xlst.iterator();
     	while (xiterator.hasNext()) 
     	{
@@ -193,6 +243,8 @@ public class Index
     	}
     	// sort again - 
     	if (ivec.size() > 0)  Collections.sort(ivec);
+    	logger.info("Returning " + ivec.size() + " Orgs records from COMBINED (text, QBE) Search");
+    	retrieved = ivec.size();
     	return ivec;
     }
     
